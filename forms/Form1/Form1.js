75dc063a3f54733b55949bd6ca4fa8710ba4a6bb
kla96587 <kla96587@creighton.edu>
2021-02-22 14:23:27 -0600

    dog name from user

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -7,9 +7,9 @@ console.log(`Hello, my name is ${myName}.`)
 console.log(`Welcome to Monday Demos`)
 
 
-/*
+
 let dogName= ""
 dogName = prompt("Enter the name of your favorite dog: ")
 console.log(`Welcome ${dogName}, it is good to have you here.`)
-*/
+
 
